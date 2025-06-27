bd8e5f6c317ddabc9c36e11333de258c012b153b
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-06-27 13:23:23 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/garlicholic/backend/GarlicHolicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.garlicholic.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarlicHolicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarlicHolicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garlicholic/backend/GarlicHolicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garlicholic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarlicHolicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
